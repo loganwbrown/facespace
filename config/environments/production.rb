30bfcdc869c16b430a5481f62427ac6c6407cc92
DPL-4 <DPL-4@DPL-4s-iMac.local>
2014-03-04 16:12:08 -0800

    improved email funcationality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Facespace::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.default_url_options = { :host => 'intense-springs-4814.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     port: '587',
     address: 'smtp.mandrillapp.com',
